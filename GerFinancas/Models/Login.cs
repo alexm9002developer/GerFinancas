75159554f8f8bf46df72f09444f3632d136f07c7
Alex Marcelo <qualityprint.atendimento@gmail.com>
2022-07-19 23:02:54 -0300

    Iniciada criação da tela de login

---
+++ GerFinancas/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GerFinancas.Models;
+
+namespace GerFinancas.Models
+{
+    public class Login
+    {
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Informe seu login!")]
+        public string LoginUsuario { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Informe sua senha!")]
+        public string Senha { get; set; }
+    }
+}
--- GerFinancas/Startup.cs
+++ GerFinancas/Startup.cs
@@ -74,7 +74,7 @@ namespace GerFinancas
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
